597f8ebf1a39e2c7efd7a9dd6dfbf2e3f92c7c10
comaerio <ohmaron88@gmail.com>
2023-09-16 17:12:47 +0900

    20230916 Editor update
    Looking for Collidable tilemap

--- Project_OurReturnedAdventure/Assets/Scenes/Main System/Characters/Character_Skills.cs
+++ Project_OurReturnedAdventure/Assets/Scenes/Main System/Characters/Character_Skills.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character_Skills : MonoBehaviour
 {
+    public GameObject SkillDB;
+    public string[] skillnames = {};
+    public int[] skills = {};
+    public float[] skillcooldowns = {}; //50 Frame = 1 Second
+    float frameper = 0.1f;
+    public int[] SkillSlots;
+    public Button[] SkillButtons = new Button[4];
     // Start is called before the first frame update
     void Start()
     {
-        
+        skillnames = new string[] {"미사용","일반 공격", };//이곳에 스킬 DB 네임 입력
+        skills = new int[] { 0, 1, };//이곳에 스킬 번호 입력
+        SkillSlots = new int[4] { 0, 0, 0, 0 }; // 0 = 미사용, 1~ = 스킬들 장착
     }
-
-    // Update is called once per frame
-    void Update()
+    void Cooldown()
     {
-        
+        foreach (int skillnum in skillcooldowns)
+        {
+            if (skillcooldowns[skillnum] > 0.02f)
+            {
+
+                skillcooldowns[skillnum] -= 0.02f;
+            }
+            else if (skillcooldowns[skillnum] > 0f)
+            {
+                skillcooldowns[skillnum] = 0f;
+            }
+        }
+    }
+    void SkillUse()
+    {
+        foreach (int skills in SkillSlots)
+        {
+            if (SkillSlots[skills] != 0)
+            {
+
+            }
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        StartCoroutine("SkillUse");
+        StartCoroutine("Cooldown");
     }
 }
